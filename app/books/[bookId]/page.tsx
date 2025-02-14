89799ba1241e08fe410f057c5015659baf5c3dc9
kkukileon305 <kkukileon305@gmail.com>
2025-02-14 16:31:46 +0900

    #feat: 해당하는 책 id가 없을시 books route로 리다이렉트 구현

--- app/books/[bookId]/page.tsx
+++ app/books/[bookId]/page.tsx
@@ -1,9 +1,19 @@
+import { getBookDetail } from "@/app/books/actions";
+import { redirect } from "next/navigation";
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ bookId: string }>;
 }) {
-  const bookId = (await params).bookId;
+  const bookId = Number((await params).bookId);
 
-  return <div>My Post: {bookId}</div>;
+  const bookDetail = await getBookDetail(bookId);
+
+  // 없는 책 요청시 redirect to books
+  if (!bookDetail) {
+    redirect("/books");
+  }
+
+  return <div>{JSON.stringify(bookDetail)}</div>;
 }
