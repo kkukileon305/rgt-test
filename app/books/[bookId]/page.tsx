ec8df40d55c9d6a48e017f96f90f3c7a0e5342ce
kkukileon305 <kkukileon305@gmail.com>
2025-02-14 22:36:28 +0900

    #feat: 상세페이지 책정보 추가

--- app/books/[bookId]/page.tsx
+++ app/books/[bookId]/page.tsx
@@ -29,7 +29,16 @@ export default async function Page({
 
       {/* 내용 */}
       <div className="h-[calc(100%-96px)] overflow-y-auto">
-        {JSON.stringify(bookDetail)}
+        <div className="p-4">
+          <p className="border border-black bg-white w-fit px-1">
+            {bookDetail.genre}
+          </p>
+        </div>
+        <h2 className="px-4 py-2 font-extrabold text-2xl">
+          {bookDetail.title}
+        </h2>
+        <p className="px-4 py-2">{bookDetail.author}</p>
+        <p className="px-4 py-2">{bookDetail.description}</p>
       </div>
 
       {/* 하단 수량 네비게이션 */}
